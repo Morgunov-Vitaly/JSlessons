fb43641fc9c1fe8dde0850e38ae24e8205b29730
Morgunov-Vitaly <Morgunov.Vitaly@gmail.com>
2018-06-06 13:22:43 +0300

    Новая серия уроков по EcmaScript6 на основе видеоуроков от Дмитрия Лаврика https://www.youtube.com/watch?v=Spme1IW1ejg&t=7s

---
+++ es6/public_html/js/js.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+"use strict";
+window.onload = function (myevent) {
+//    console.log("My event:");
+//    console.log(myevent);
+//    for (var i = 0; i <= 10; i++) {
+//
+//    }
+//    for (let j = 0, max = 10; j < max; j++) {
+//        
+//    }
+    /* Демонстрируем отличия в области видимости var vs let */
+//console.log(typeof(i));
+//console.log(typeof(j));
+time();
+time(50);
+}
+/* Новая возможность задания значений по-умолчанию
+ Раньше для этого было необходимо писать так:
+ var timer = (typeOf(t) ==="undefined")? 60: t; 
+ т.к. короткая версия  var timer = t || 60;
+  не позволяла передавать 0 */
+function time(t=60){
+    console.log(t);
+}
